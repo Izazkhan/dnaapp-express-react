a90646f1f653e886381908bf24942394ef18e779
IzazKhan <izazkhan134@gmail.com>
2025-10-29 00:52:50 +0500

    Migrations config added

--- src/app/database/migrations/2025_10_28_create_users_table.js
+++ src/app/database/migrations/2025_10_28_create_users_table.js
@@ -1,22 +1,23 @@
-export default {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.sequelize.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        email VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-        CONSTRAINT valid_email_format
-      );
+'use strict';
 
-      CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
-    `);
-  },
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id         INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            email      VARCHAR(255) NOT NULL UNIQUE,
+            password   VARCHAR(255) NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+        );
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.sequelize.query(`
-      DROP TABLE IF EXISTS users CASCADE;
+        CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
     `);
-  }
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.sequelize.query(`
+        DROP TABLE IF EXISTS users CASCADE;
+    `);
+    }
 };
