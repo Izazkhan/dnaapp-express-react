8dd0e2e431ae6be7b9f2d102d6820f378b7faedf
IzazKhan <izazkhan134@gmail.com>
2025-10-30 00:36:20 +0500

    In progress..

--- src/app.js
+++ src/app.js
@@ -16,7 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // Security middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 
 // Logging middleware
